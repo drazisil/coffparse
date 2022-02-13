a288993ba88a79775fdb9b482914e67b6caf9b49
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-02-13 13:38:17 -0500

    cleaner loop

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import assert from "assert";
 import { readFileSync } from "fs";
 
-export function readByte2(buffer: Buffer, index:number) {
-    return buffer.slice(index, index+2)
+export function readByte2(buffer: Buffer, index: number) {
+    return buffer.slice(index, index + 2)
 }
 
-export function readByte4(buffer: Buffer, index:number) {
-    return buffer.slice(index, index+4)
+export function readByte4(buffer: Buffer, index: number) {
+    return buffer.slice(index, index + 4)
+}
+
+export function checkPESig(bytes: Buffer): boolean {
+    const PE_SIG = Buffer.from(['P', 'E', String.fromCharCode(0x00), String.fromCharCode(0x00)].join(''))
+    return bytes.toString('hex') === PE_SIG.toString('hex')
+}
+
+export function checkPEOffset(buf: Buffer, counter: number): boolean {
+    const peOffset = buf.readInt32LE(0x3c)
+    return counter === peOffset
 }
 
 const buf = readFileSync('./mcity.exe')
@@ -14,27 +24,24 @@ const buf = readFileSync('./mcity.exe')
 let counter = 0
 const STEP = 4
 const MAX_COUNT = 512
-const PE_SIG = Buffer.from(['P', 'E', String.fromCharCode(0x00), String.fromCharCode(0x00)].join(''))
-
-console.log(PE_SIG.toString("hex"))
 
 while (counter <= MAX_COUNT) {
     const byte = readByte4(buf, counter)
 
-    console.log(byte.toString("hex"))
-    if (byte.toString('hex') === PE_SIG.toString('hex') ) {
-        console.log(counter)
-        console.log(byte.toString("hex"))        
-        console.log(byte.toString())
+    if (checkPESig(byte)) {
 
-        const peIndex = counter
-        const peOffset = buf.readInt32LE(0x3c)
+        console.log(`PR Header located at offset ${counter.toString(16)}`)
 
-        assert.strictEqual(peIndex, peOffset, "the PE offset did not match what 0x3c said")
+        if (counter !== 0) {
+            if (checkPEOffset(buf, counter) != true) {
+                throw new Error("The address at 0x3c does not match the location to the PR header");
+            }
+
+        }
+
         break
     }
 
 
     counter = counter + STEP
 }
-
